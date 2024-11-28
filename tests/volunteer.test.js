26e19b9f4dbc311e5e4f5e15a6240d5833e7e3ed
Corey Wojcik <cwojcik2@academic.rrc.ca>
2024-11-28 07:25:12 -0600

    Just started tests

---
+++ tests/volunteer.test.js
@@ -0,0 +1,16 @@
+const { validateCharityName, validateHours, validateDate, validateStarRating } = require("../volunteer.js");
+
+const { JSDOM } = require("jsdom");
+
+test("input validation: charity name has at least 1 character", () => {
+    // AAA ("triple-a") pattern
+    // arrange
+    
+
+    // act
+    
+
+    // assert
+    // all "expect" invocations must be true for the test to pass
+    
+});
